81b6044865a4c89efa020cb0d5e2a578f6943a54
sminha <sminha8590@naver.com>
2025-09-26 19:21:53 +0900

    chore: Add Jest configuration

---
+++ client/jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: { 
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
+   },
+  transform: { "^.+\\.tsx?$": "ts-jest" },
+};
