038975cdad03e1f74a1b9441c742ce65e3cd203e
sminha <sminha8590@naver.com>
2025-10-05 04:42:51 +0900

    chore: remove redux-persist

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,16 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import signupReducer from './slices/signupSlice.ts';
 import userReducer from './slices/userSlice.ts';
 import purchaseReducer, { setTestData } from './slices/purchaseSlice.ts';
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['signup', 'user', 'purchase'],
-};
 
 const rootReducer = combineReducers({
   signup: signupReducer,
@@ -18,10 +10,8 @@ const rootReducer = combineReducers({
   purchase: purchaseReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
@@ -31,8 +21,6 @@ export const store = configureStore({
 if (process.env.NODE_ENV === 'development') {
   store.dispatch(setTestData(1000));
 }
-
-export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
